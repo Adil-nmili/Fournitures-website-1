8a11a0841d23358f09d592c2b483be882eb72590
Adil Nmili <128920526+Adil-nmili@users.noreply.github.com>
2025-07-27 13:36:33 +0000

    initial the forniture project

---
+++ Client/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className="min-h-screen bg-based-light">
+      <header className="bg-based-blue text-white">
+        {/* Header content */}
+        <h1>hello world</h1>
+      </header>
+      
+      <main className="container mx-auto py-8">
+        {/* Page content */}
+      </main>
+      
+      <footer className="bg-based-blue text-white py-6">
+        {/* Footer content */}
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ Client/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        // Primary colors
+        'based-red': '#DD1D25',
+        'based-blue': '#104C64',
+        
+        // Neutral colors
+        'based-light': '#C6C6D0',
+        
+        // Accent colors (wood tones)
+        'based-wood-light': '#D59D80',
+        'based-wood-medium': '#C0754D',
+        'based-wood-dark': '#B6410F'
+      },
+    },
+  },
+  plugins: [],
+}
